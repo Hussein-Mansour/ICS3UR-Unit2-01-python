3725e6e90e69d44a5fa29d297061efa8dd9692bb
Hussein-Mansour <hussein.mansour@ocsbstudent.ca>
2021-04-26 14:21:05 +0000

    added in basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Hussein Mansour
+# Created on: Sun,Apr26/2020
+# This program does basic math
+
+
+def main():
+    
+
+
+if __name__ == "__main__":
+    main()
